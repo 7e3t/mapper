f20f1b6092e5a05df127c8416125859b74d3c000
alii <7e3t>
2019-04-09 16:39:06 +0430

    add new test to RunDemo for primary key

--- src/ir/ali/mapper/RunDemo.java
+++ src/ir/ali/mapper/RunDemo.java
@@ -3,6 +3,7 @@ package ir.ali.mapper;
 import ir.ali.mapper.dto.PersonDto;
 import ir.ali.mapper.dto.PhoneDto;
 import ir.ali.mapper.entity.Person;
+import ir.ali.mapper.entity.Phone;
 import ir.ali.mapper.entity.PhoneType;
 
 import java.util.ArrayList;
@@ -34,9 +35,25 @@ public class RunDemo {
         System.out.println(person);
 
         person.setName("rez");
+
+        person.getPhones().get(1).setType(PhoneType.MOBILE);
+        person.getPhones().remove(0);
+
+        Phone phone3 = new Phone();
+        phone3.setNumber("+98930");
+        phone3.setType(PhoneType.MOBILE);
+
+        Phone phone4 = new Phone();
+        phone4.setNumber("+98937");
+        phone4.setType(PhoneType.MOBILE);
+
+        person.getPhones().add(phone3);
+        person.getPhones().add(phone4);
+
         System.out.println(person);
 
         Mapper.map(person, personDto);
+
         System.out.println(person);
     }
 }
